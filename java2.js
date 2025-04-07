08fa7cbf524582112635a3b6ea60492de399a549
edvardsk06 <152157821+edvardsk06@users.noreply.github.com>
2025-04-07 11:12:01 +0300

    Update java2.js

--- java2.js
+++ java2.js
@@ -1,1 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const spuldze = document.querySelector(".w");
+    const spuldze1 = document.querySelector(".a");
 
+    function parslēgtGaismu(spuldze) {
+        if (spuldze.style.backgroundImage.includes("on")) {
+            spuldze.style.backgroundImage = "url('bildes/light-bulb-off.png')";
+        } else {
+            spuldze.style.backgroundImage = "url('bildes/light-bulb-on.png')";
+        }
+    }
+
+    spuldze.addEventListener("click", function() {
+        parslēgtGaismu(spuldze);
+    });
+
+    spuldze1.addEventListener("click", function() {
+        parslēgtGaismu(spuldze1);
+    });
+});
