3521390b2d3331904698a8537792851f8f211b41
edvardsk06 <152157821+edvardsk06@users.noreply.github.com>
2025-04-11 23:08:35 +0300

    Update masivs.js

--- masivs.js
+++ masivs.js
@@ -1,20 +1,18 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const spuldze = document.querySelector(".w");
-    const spuldze1 = document.querySelector(".a");
+const jaut = [
+    { jau: `Jūra Ziemeļeiropā, kura tiek uzskatīta par "saldāko" jūru pasaulē, tās vidējais sāļums ir tikai 8-10 promiles- ... jūra.`, atb: "Baltijas" },
+    { jau: "Konstitucionāla monarhija, federāla valsts, kurai pieder divas autonomas pilsētas Āfrikas kontinentā.", atb: "Spānija" },
+    { jau: "Šaurums, zem kura uzbūvēts Eirotunelis- ... šaurums.", atb: "Lamanša" },
+    { jau: "Pundurvalsts Eiropā- teokrātiska monarhija.", atb: "Vatikāns" },
+    { jau: "Kura no šīm nav Zemes iekšējās uzbūves sastāvdaļa-kodols, astenosfēra, pedosfēra", atb: "pedosfēra" },
+    { jau: "Kilimandžāro atrodas...(valsts).", atb: "Tanzānijā" },
+    { jau: "Katalonija atrodas...(valsts).", atb: "Spānijā" },
+    { jau: "Kura no šīm nav Latīņamerikas valsts?(Bolīvija,Venecuēla, Urugvaja, Tobago) ", atb: "Tobago" },
+    { jau: "Kā sauc nozari, kas pētī kalnu izcelšanos?(Paleoģenēze, Oroģenēze, Oroloģija, Orometrija", atb: "Oroloģija" },
+    { jau: "Kura ir garākā upe Āzijā? ", atb: "Jandzi" },
+    { jau: "Cik okeāniem iet cauri ekvators?(uzraksti ciparu)", atb: "3" },
+    { jau: "Patiess vai nepatiess, Klusais okeāns ir lielāks par Mēnesi?", atb: "Patiess" },
+    { jau: "Pilsēta, kurā ir vislielākais iedzīvotāju blīvums pasaulē?", atb: "Manila" },
+    { jau: "Kurā no šo valstu karogiem ir attēlots zelta putns ar sauli?(Kirgizstāna, Kiribati, Santome un Prinsipi, Nauru) ", atb: "Kiribati" },
 
-    function parslēgtGaismu(spuldze) {
-        if (spuldze.style.backgroundImage.includes("on")) {
-            spuldze.style.backgroundImage = "url('bildes/light-bulb-off.png')";
-        } else {
-            spuldze.style.backgroundImage = "url('bildes/light-bulb-on.png')";
-        }
-    }
+];
 
-    spuldze.addEventListener("click", function() {
-        parslēgtGaismu(spuldze);
-    });
-
-    spuldze1.addEventListener("click", function() {
-        parslēgtGaismu(spuldze1);
-    });
-});
