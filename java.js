8bc985dd958ab58d4ab9fde98bb8c8bf05f35f7a
edvardsk06 <152157821+edvardsk06@users.noreply.github.com>
2025-04-11 23:07:48 +0300

    Update java.js

--- java.js
+++ java.js
@@ -1,21 +1,64 @@
-let datumsLidz = new Date("May 20, 2025 00:00:00").getTime();
+
+const datumsLidz = new Date("May 20, 2025 00:00:00").getTime();
+
+function atjauno() {
+  const tagad = new Date().getTime();
+
+  const laiksPalicis = datumsLidz - tagad;
+
+   const dienas = Math.floor(laiksPalicis / (1000 * 60 * 60 * 24));
+  const stundas = Math.floor((laiksPalicis % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((laiksPalicis % (1000 * 60 * 60)) / (1000 * 60));
+const sekundes = Math.floor((laiksPalicis % (1000 * 60)) / 1000);
+
+  const laikaTeksts = `${dienas}d ${stundas}h ${minutes}m ${sekundes}s`;
+  document.getElementById("Laiks").innerHTML = laikaTeksts;
+}
+setInterval(atjauno, 1000);
 
 
-let x = setInterval(function() {
-let tagad = new Date().getTime();
-let laiksPalicis = datumsLidz - tagad
 
-let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+let kurs = Math.floor(Math.random() * jaut.length);
+document.getElementById("jautajums").innerHTML = jaut[kurs].jau;
 
-document.getElementById("izvade").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+function poga() {
 
-if (laiksPalicis < 0) {  
- clearInterval(x);
-  document.getElementById("izvade").innerHTML = "Laiks beidzies";
+const ievade = document.getElementById("atbildetais").value.trim().toLowerCase();
+const pareiza = jaut[kurs].atb.toLowerCase();
+  const izvade = document.getElementById("izvade");
+
+
+if (ievade === pareiza) {
+    izvade.innerHTML = 'Pareizi!';
   }
-}, 1000);
+ else {
+    izvade.innerHTML = `Nepareizi! Pareizā atbilde ir: ${pareiza}.`;
+  }
+
+  setTimeout(() => {
+    izvade.innerHTML = "";
+    kurs = Math.floor(Math.random() * jaut.length);
+
+    document.getElementById("jautajums").innerHTML = jaut[kurs].jau;
+  }, 2000);
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  const spuldze = document.querySelector(".w");
+
+  function parslēgt(spuldze) {
+    if (spuldze.style.backgroundImage.includes("gep.jpg")) {
+      spuldze.style.backgroundImage = "url('bildes/jautajumi.jpg')";
+    } else {
+      spuldze.style.backgroundImage = "url('bildes/gep.jpg')";
+    }
+  }
+
+  spuldze.addEventListener("click", function() {
+    parslēgt(spuldze);
+  });
+});
+
+
 
